88f56a714a51a8ee9eca7110953f63ac9ef580a1
Eivinas Butkus <eivinas.butkus@gmail.com>
2019-12-11 12:19:19 -0500

    script to copy the updated modules without rebuilding

---
+++ copy_modules.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker cp workshop/module_1.ipynb gen:/gen-quickstart/workshop/module_1.ipynb
+docker cp workshop/module_2.ipynb gen:/gen-quickstart/workshop/module_2.ipynb
+docker cp workshop/module_3.ipynb gen:/gen-quickstart/workshop/module_3.ipynb
